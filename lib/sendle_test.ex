518067f8c338b59e109377e54bf487bb984eecab
Jared Hughes <j@redhugh.es>
2020-01-29 11:28:39 +1100

    chore(*): Initialise exercise

---
+++ lib/sendle_test.ex
@@ -0,0 +1,17 @@
+defmodule SendleCodeTest do
+  @moduledoc """
+  Documentation for SendleTest.
+  """
+
+  def new(_opts \\ []) do
+
+  end
+
+  def get_quotes(_pricing_model, _shipments) do
+
+  end
+
+  def print_quotes(_quotes) do
+
+  end
+end
